ddd8f3db53ecfe5b9f14dbd943b3fad143b2dfce
Kenny Vo <kennyvo91@gmail.com>
2018-06-17 22:42:06 -0500

    sale conditional added

--- src/ProductContainer.js
+++ src/ProductContainer.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import ProductList from './ProductList';
+import './productContainer.css';
+
 
 class ProductContainer extends Component {
 
   render(){
       return (
-        <div className="productContainer container">
+        <div className="productContainer container-fluid">
           <ProductList/>
         </div>
       )
